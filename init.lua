f9c6613b1ffd3960d6bdee2cf9bdcd327ae63f57
Zachery Johnson <zacheryjohnson13@gmail.com>
2023-07-05 20:34:24 -0700

    Add LSP commands

--- init.lua
+++ init.lua
@@ -1,1 +1,13 @@
 require('zjohnson')
+
+local rt = require("rust-tools")
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-CR>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>q", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
--- lua/zjohnson/packer.lua
+++ lua/zjohnson/packer.lua
@@ -19,6 +19,7 @@ return require('packer').startup(function(use)
       {'hrsh7th/nvim-cmp'},     -- Required
       {'hrsh7th/cmp-nvim-lsp'}, -- Required
       {'L3MON4D3/LuaSnip'},     -- Required
+      {'simrat39/rust-tools.nvim'}
     }
   }
 
